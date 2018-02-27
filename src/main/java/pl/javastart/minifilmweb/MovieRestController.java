6c98cc410ad2ffa44df91ca91a648e3ac8a036ad
Mateusz <mateuszhotowy@gmail.com>
2018-02-27 11:53:47 +0100

    Done.

--- src/main/java/pl/javastart/minifilmweb/MovieRestController.java
+++ src/main/java/pl/javastart/minifilmweb/MovieRestController.java
@@ -1,8 +1,8 @@
 package pl.javastart.minifilmweb;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,6 +16,33 @@ public class MovieRestController {
     public List<Movie> getAllMovies() {
         System.out.println("Odpytanie o wszystkie filmy");
         return movieRepository.findAll();
+    }
+
+    @GetMapping("/api/movies/{id}")
+    public Movie getMovie(@PathVariable Long id){
+        return movieRepository.findOne(id);
+    }
+
+    @PostMapping("/api/movies")
+    public ResponseEntity<?> addMovie(@RequestBody Movie movie){
+        movieRepository.save(movie);
+        return ResponseEntity.status(201).build();
+    }
+
+    @PutMapping("/api/movies/{id}")
+    public void updateMovie(@PathVariable Long id,
+                            @RequestBody Movie movie){
+        Movie one = movieRepository.findOne(id);
+        one.setActors(movie.getActors());
+        one.setPremiereDate(movie.getPremiereDate());
+        one.setTitle(movie.getTitle());
+        movieRepository.save(one);
+
+    }
+
+    @DeleteMapping("/api/movies/{id}")
+    public void deleteMovie(@PathVariable Long id){
+        movieRepository.delete(id);
     }
 
 }
